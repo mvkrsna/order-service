ab0b56d13358abd70d80d4b407383a50354d02e0
mvkrsna <krsna.genai@gmail.com>
2025-11-09 09:43:59 -0600

    Order Service along with Unit tests using Junit5 and mockito

--- src/test/java/com/techbud/orderService/service/OrderServiceTest.java
+++ src/test/java/com/techbud/orderService/service/OrderServiceTest.java
@@ -2,14 +2,14 @@ package com.techbud.orderService.service;
 
 import com.techbud.orderService.entity.Order;
 import com.techbud.orderService.repository.OrderRepository;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -28,10 +28,10 @@ public class OrderServiceTest {
         Order order = Order.builder().id(1L).name("Test Order").price(150.0).build();
         when(orderRepository.getOrderById(order.getId())).thenReturn(order);
         Order expectedOrderInformation = orderService.getOrderById(order.getId());
-        Assertions.assertNotNull(expectedOrderInformation);
-        Assertions.assertEquals(order.getId(), expectedOrderInformation.getId());
-        Assertions.assertEquals(order.getName(), expectedOrderInformation.getName());
-        Assertions.assertEquals(order.getPrice(), expectedOrderInformation.getPrice());
+        assertNotNull(expectedOrderInformation);
+        assertEquals(order.getId(), expectedOrderInformation.getId());
+        assertEquals(order.getName(), expectedOrderInformation.getName());
+        assertEquals(order.getPrice(), expectedOrderInformation.getPrice());
     }
 
     // Negative flow
@@ -40,6 +40,6 @@ public class OrderServiceTest {
         Order order = Order.builder().id(1L).name("Test Order").price(150.0).build();
         when(orderRepository.getOrderById(order.getId())).thenReturn(order);
         Order expectedOrderInformation = orderService.getOrderById(1L);
-        Assertions.assertNotNull(expectedOrderInformation);
+        assertNotNull(expectedOrderInformation);
     }
 }
